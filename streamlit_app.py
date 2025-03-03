c64503cebe0e5142eec88c36ab7bf74f7b6bfefb
omer5131 <omer.bar31@gmail.com>
2025-03-03 03:02:44 +0000

    Initial commit of Streamlit Shipment Price Calculator

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ st.title("Shipment Freight Price Calculator")
 # Input fields for the four inputs
 total_items_cost = st.number_input("Total Items Cost in USD", value=0.0, step=0.1, format="%.2f")
 conversion_rate = st.number_input("Currenct Conversion Rate", value=0.0, step=0.1, format="%.5f")
-customs_rate = st.number_input("Custom '%' duty ", value=0.12, step=0.01, min_value=0.0, max_value=1.0, format="%.2f")
+customs_rate = st.number_input("Custom % duty ", value=0.12, step=0.01, min_value=0.0, max_value=1.0, format="%.2f")
 tax_rate = st.number_input(" Freight Percentage (from Israel, in percentiles)", value=0.27, step=0.01, min_value=0.0, max_value=1.0, format="%.2f")
 total_taxes = st.number_input("Total Taxes in DKK", value=0.0, step=0.1, format="%.2f")
 
